2fa59a4a5ebd97dc2e01d72eb11f6e68d0954313
Giovanni Andrade <giovanni.andrade9991@gmail.com>
2020-11-21 23:50:54 -0300

    Component interaction working fine

--- src/app/currency-selector/currency-selector.component.ts
+++ src/app/currency-selector/currency-selector.component.ts
@@ -1,14 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, Output, EventEmitter, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-currency-selector',
+  selector: 'simple-currency-selector',
   templateUrl: './currency-selector.component.html',
-  styleUrls: ['./currency-selector.component.scss'],
+  styleUrls: ['./currency-selector.component.css']
 })
 export class CurrencySelectorComponent implements OnInit {
 
-  constructor() { }
+  @Input()
+  currencyCodes: string[];
 
-  ngOnInit() {}
+  @Output()
+  selectedCurrency: EventEmitter<string> = new EventEmitter<string>();
+
+  currentCurrencyCode: string;
+
+  ngOnInit() {
+    this.currentCurrencyCode = this.currencyCodes[0];
+  }
+
+  onChange(event: Event) {
+    this.currentCurrencyCode = (<HTMLInputElement>event.target).value;
+    this.selectedCurrency.emit(this.currentCurrencyCode);
+  }
 
 }
